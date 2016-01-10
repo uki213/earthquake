83f0fb0934619d783bea444db17b6007feed7f79
uki_213 <niimiyouki@gmail.com>
2016-01-10 21:00:14 +0900

    jsLint対応

--- js/$.earthquake.js
+++ js/$.earthquake.js
@@ -1,35 +1,38 @@
 /*global jQuery, MutationObserver */
 (function ($) {
-    $.fn.earthquake = function (config) {
-        // 変数設定
-        var dom = $(this);
-        var defaults = {
-            eventName: 'earthquake'
-        };
-        var options = $.extend(defaults, config);
+	'use strict';
+	$.fn.earthquake = function (config) {
+		// 変数設定
+		var dom, defaults, options;
+		dom = $(this);
+		defaults = {
+			eventName: 'earthquake'
+		};
+		options = $.extend(defaults, config);
 
-        // 関数設定
-        function earthquake() {
-            if (typeof MutationObserver === 'function') {
-                var mo = new MutationObserver(changeDom);
-                mo.observe(
-                    dom[0], {
-                        childList: true,
-                        subtree: true
-                    }
-                );
-            } else {
-                dom.on('DOMSubtreeModified', function () {
-                    changeDom();
-                });
-            }
-        }
+		// 関数設定
+		function changeDom() {
+			dom.trigger(options.eventName);
+		}
 
-        function changeDom () {
-            dom.trigger(options.eventName);
-        }
+		function earthquake() {
+			if (typeof MutationObserver === 'function') {
+				var mo = new MutationObserver(changeDom);
+				mo.observe(
+					dom[0],
+					{
+						childList: true,
+						subtree: true
+					}
+				);
+			} else {
+				dom.on('DOMSubtreeModified', function () {
+					changeDom();
+				});
+			}
+		}
 
-        // 実行
-        earthquake();
-    };
-})(jQuery);
+		// 実行
+		earthquake();
+	};
+}(jQuery));
